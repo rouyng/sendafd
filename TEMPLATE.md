da56db879080817ef45a8f3e53f0f30a0c6fdc3e
rouyng <54250428+rouyng@users.noreply.github.com>
2023-02-28 09:11:08 -0700

    begin email rendering module in renderer.py, create placeholder documentation and placeholder default template

---
+++ TEMPLATE.md
@@ -0,0 +1,15 @@
+# sendafd Template documentation
+This document describes the templating format used by sendafd to generate emails. sendafd uses [Jinja](https://jinja.palletsprojects.com/en/3.1.x/) as the templating engine.
+
+# Variables
+
+## 
+
+## `sections`
+List of `Section` objects. Each `Section` object has `Section.name` and `Section.body` attributes.
+
+
+# Examples
+```html
+<!--example template syntax goes here-->
+```
--- sendafd/renderer.py
+++ sendafd/renderer.py
@@ -3,7 +3,22 @@ Renders email templates. Takes forecast text from apiclient.py as input and outp
 or plaintext.
 """
 
-import jinja2
+from jinja2 import Environment, PackageLoader, select_autoescape
 import logging
 
 logger = logging.getLogger(__name__)
+
+# create Jinja Environment
+env = Environment(
+    loader=PackageLoader("sendafd"),
+    autoescape=select_autoescape()
+)
+
+def render_email(afd: apiclient.AreaForecastDiscussion, template_path: str = None):
+    """Render email as plaintext or html using specified jinja template"""
+    if template_path:
+        template = env.get_template(template_path)
+        return template.render() # TODO: pass variables to template
+    else:
+        logger.debug("No template path provided, generating plaintext email")
+        return afd.raw_text
